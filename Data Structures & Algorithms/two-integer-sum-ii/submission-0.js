98c5f07cb413912a938c2065d14d5452feac5686
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-10 20:00:04 +0500

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.js
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(arr, target) {
+        let i = 0;
+        let j = arr.length - 1;
+
+        while(i < j){
+            let sum = arr[i] + arr[j];
+            if(sum > target){
+                --j;
+            }
+            else if(sum < target){
+                ++i;
+            }
+            else{
+                return [i+1, j+1]
+            }
+        }
+    }
+}
