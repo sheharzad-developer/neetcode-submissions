bbd1d63351a5e1ad2467e5ce0839d4801668c3df
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-11 16:59:23 +0500

    Add: three-integer-sum - submission-0

---
+++ Data Structures & Algorithms/three-integer-sum/submission-0.js
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(arr) {
+        arr.sort((a,b) => a-b);
+            let ans = [];
+            for(let i = 0; i < arr.length; i++){
+                if(arr[i] != arr[i-1]){
+                    twoSum(arr, i, ans);
+                }
+            }
+        return ans;
+    }
+}
+
+var twoSum = function(arr, x, ans) {
+    let i = x + 1;
+    let j = arr.length - 1;
+
+    while(i < j){
+        let sum = arr[i] + arr[j] + arr[x];
+        if(sum > 0){
+            --j;
+        }
+        else if(sum < 0){
+            ++i;
+        }
+        else{
+            ans.push([arr[i], arr[j], arr[x]]), ++i, --j;
+            while(i < j && arr[i] == arr[i-1]) ++i;
+        }
+    }
+};
