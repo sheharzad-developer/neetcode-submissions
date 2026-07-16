2373d50f0e1ca1276e28f3dc2c320f7af34f3dbc
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-16 20:37:30 +0500

    Add: daily-temperatures - submission-0

---
+++ Data Structures & Algorithms/daily-temperatures/submission-0.js
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(arr) {
+        let stack = []; 
+        let n = arr.length;
+        let ans = Array(n).fill(0);
+
+        stack.push([n-1]);
+        ans[n-1]=0;
+
+        for(let i = n-2; i>=0; i--){
+            while(stack.length){
+                let top = stack[stack.length-1];
+                if(arr[i] >= arr[top]){
+                    stack.pop();
+                }
+                else {
+                    ans[i] = top -i;
+                    break;
+                }
+            }
+            if(!stack.length){
+                ans[i] = 0;
+            }
+            stack.push(i);
+        }
+
+        return ans;
+    }
+}
