c2fed720e2ab78431910b2f2ace7b3e0f5442a2b
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-17 16:39:50 +0500

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        for (let left = 0, right = nums.length - 1; left <= right;) {
+        let middle = Math.floor((left + right) / 2);
+
+        if (nums[middle] === target) {
+            return middle;
+        } else if (nums[middle] < target) {
+            left = middle + 1;
+        } else {
+            right = middle - 1;
+        }
+    }
+    return -1;
+    }
+}
