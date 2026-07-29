20e9d9e08379dcc2b98484c00fe1e3dd7bcefb6a
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-29 22:08:59 +0500

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.js
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let min = prices[0];
+        let maxProfit = 0;
+
+        for(let i = 0; i < prices.length; i++){
+            if(prices[i] - min > maxProfit){
+                maxProfit = prices[i]-min;
+            }
+            if(prices[i]< min){
+                min = prices[i];
+            }
+        }
+        return maxProfit;
+    }
+}
