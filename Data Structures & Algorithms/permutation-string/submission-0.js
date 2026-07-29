1bda6c00a6e782e50a37fbfb7e7f4c7e27eb7555
Sheharzad <sheharzad.salahuddin9000@outlook.com>
2026-07-29 22:13:48 +0500

    Add: permutation-string - submission-0

---
+++ Data Structures & Algorithms/permutation-string/submission-0.js
@@ -0,0 +1,46 @@
+class Solution {
+    /**
+     * @param {string} s1
+     * @param {string} s2
+     * @return {boolean}
+     */
+    checkInclusion(s1, s2) {
+        if (s1.length > s2.length) return false;
+
+        let hashS = Array(26).fill(0);
+        let hashW = Array(26).fill(0);
+        
+        for(let i = 0; i < s1.length; i++) {
+            hashS[s1.charCodeAt(i) - 97]++;
+            hashW[s2.charCodeAt(i) - 97]++;
+        }
+
+        let i = 0;
+        let j = s1.length - 1;
+
+        while(j < s2.length){
+            if(isHashSame(hashS, hashW)){
+                return true;
+            }
+
+            hashW[s2.charCodeAt(i) - 97]--;
+            i++;
+            j++;
+
+            if(j < s2.length){
+                hashW[s2.charCodeAt(j) - 97]++;
+            }
+        }
+        return false;
+        }
+}
+
+var isHashSame = function(hashS, hashW){
+    for(let i = 0; i < 26; i++){
+        if(hashS[i] !== hashW[i]){
+            return false;
+        }
+    }
+
+    return true;
+}
